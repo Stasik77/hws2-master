5b62298aa9fef29eabcab54e3cf3b40b88820087
stas <staz86@mail.ru>
2026-03-20 19:31:05 +0300

    1

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -10,78 +10,87 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const start = () => {
-
-        // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
-        // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-
+        const id = setInterval(() => {
+            setDate(new Date());
+        }, 1000)
+        setTimerId(+id)
     }
 
-    const stop = () => {
-        // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
+    // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
+    // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
 
-    }
 
-    const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
 
-    }
-    const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
+const stop = () => {
+    clearInterval(timerId)
+    setTimerId(undefined)
 
-    }
 
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
 
-    // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+}
 
-    return (
-        <div className={s.clock}>
-            <div
-                id={'hw9-watch'}
-                className={s.watch}
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                <span id={'hw9-day'}>{stringDay}</span>,{' '}
-                <span id={'hw9-time'}>
+const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
+    setShow(true)
+}
+const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
+    setShow(false)
+}
+
+const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+
+// день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
+const stringDay =  date.getDay() || <br/> // пишут студенты
+const stringMonth = 'date->month' || <br/> // пишут студенты
+
+return (
+    <div className={s.clock}>
+        <div
+            id={'hw9-watch'}
+            className={s.watch}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+        >
+            <span id={'hw9-day'}>{stringDay}</span>,{' '}
+            <span id={'hw9-time'}>
                     <strong>{stringTime}</strong>
                 </span>
-            </div>
+        </div>
 
-            <div id={'hw9-more'}>
-                <div className={s.more}>
-                    {show ? (
-                        <>
-                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
-                            <span id={'hw9-date'}>{stringDate}</span>
-                        </>
-                    ) : (
-                        <>
-                            <br/>
-                        </>
-                    )}
-                </div>
-            </div>
-
-            <div className={s.buttonsContainer}>
-                <SuperButton
-                    id={'hw9-button-start'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер запущен
-                    onClick={start}
-                >
-                    start
-                </SuperButton>
-                <SuperButton
-                    id={'hw9-button-stop'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер не запущен
-                    onClick={stop}
-                >
-                    stop
-                </SuperButton>
+        <div id={'hw9-more'}>
+            <div className={s.more}>
+                {show ? (
+                    <>
+                        <span id={'hw9-month'}>{stringMonth}</span>,{' '}
+                        <span id={'hw9-date'}>{stringDate}</span>
+                    </>
+                ) : (
+                    <>
+                        <br/>
+                    </>
+                )}
             </div>
         </div>
-    )
+
+        <div className={s.buttonsContainer}>
+            <SuperButton
+                id={'hw9-button-start'}
+                disabled={!!timerId} // пишут студенты // задизэйблить если таймер запущен
+                onClick={start}
+            >
+                start
+            </SuperButton>
+            <SuperButton
+                id={'hw9-button-stop'}
+                disabled={!timerId} // пишут студенты // задизэйблить если таймер не запущен
+                onClick={stop}
+            >
+                stop
+            </SuperButton>
+        </div>
+    </div>
+)
 }
 
 export default Clock
