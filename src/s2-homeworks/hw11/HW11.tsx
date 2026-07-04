a78c387650f3c7ad3de3ad7711308105ac764d4c
stas <staz86@mail.ru>
2026-07-04 22:02:11 +0300

    1

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -18,8 +18,8 @@ function HW11() {
     const change = (event: any, value: any) => {
 
             if(Array.isArray(value)) {
-            setValue1(value[0]);
-            setValue2(value[0]);
+            setValue1(value1);
+            setValue2(value2);
         } else {
             setValue1(value);
         }
@@ -38,6 +38,7 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
+
  onChange={change}
                             // сделать так чтоб value1 изменялось // пишет студент
                         />
@@ -48,6 +49,7 @@ function HW11() {
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
                             onChange={change}
+
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
